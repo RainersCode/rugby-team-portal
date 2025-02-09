a90cc82afb95ed8d0c3ccd1879d8e0210fc587b3
Rainers Lovkins <rainerslovkins@gmail.com>
2025-02-09 12:26:49 +0200

    Fonts change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jockey_One } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer/Footer";
 import { ThemeProvider } from "@/components/shared/ThemeProvider";
 import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ["latin"] });
+const jockeyOne = Jockey_One({ 
+  weight: '400',
+  subsets: ["latin"],
+  variable: '--font-jockey-one',
+});
 
 export const metadata: Metadata = {
   title: "Rugby Team Portal",
@@ -21,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.className} bg-bg-light dark:bg-bg-dark text-content-light dark:text-content-dark`}
+        className={`${jockeyOne.variable} bg-bg-light dark:bg-bg-dark text-content-light dark:text-content-dark`}
       >
         <ThemeProvider>
           <div className="flex flex-col min-h-screen">
